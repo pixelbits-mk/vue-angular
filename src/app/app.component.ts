6a6eea14c914503b5889fab8e7af43ae4d080619
unknown <kmike@vyana.io>
2019-11-20 21:16:24 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, CUSTOM_ELEMENTS_SCHEMA, Input } from '@angular/core';
+import VCalendar from 'v-calendar';
+declare const Vue: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app12';
+export class AppComponent implements OnInit {
+  text = 'hello there how are you?';
+  ngOnInit() {
+    const text = this.text;
+    const MyVueWebComp = {
+      props: [],
+      template: `
+      <v-calendar  :attributes='attrs'></v-calendar>
+      `,
+      data() {
+        return {
+          selectedDate: null,
+          mode: 'single',
+          attrs: [
+            {
+              key: 'today',
+              highlight: true,
+              popover: {
+                label: text,
+              },
+              dates: new Date()
+            },
+          ]
+        };
+      },
+      methods: {
+      }
+    };
+    Vue.customElement('vue-calendar', MyVueWebComp);
+  }
 }
