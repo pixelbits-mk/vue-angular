f80ae44d8edeb815151ae4891bdb64bfa76e65c7
unknown <kmike@vyana.io>
2019-11-20 23:37:11 -0500

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,83 +15,77 @@ export class AppComponent implements OnInit {
     const MyVueWebComp = {
       props: [],
       template: `
-      <div>
-      <v-app id="inspire">
-      <v-row>
-        <v-col>
-        <table>
-        <tr><td>
-        <v-calendar  :attributes='attrs'></v-calendar>
-        </td></tr>
-        <tr>
-          <td>
-          <v-menu
-          ref="menu"
-          v-model="menu2"
-          :close-on-content-click="false"
-          :nudge-right="40"
-          :return-value.sync="time"
-          transition="scale-transition"
-          offset-y
-          max-width="290px"
-          min-width="290px"
-        >
-          <template v-slot:activator="{ on }">
-            <v-text-field
-              v-model="time"
-              label="Picker in menu"
-              prepend-icon="access_time"
-              readonly
-              v-on="on"
-            ></v-text-field>
-          </template>
-          <v-time-picker
-            v-if="menu2"
+      <v-app id="calendar">
+      <v-container id="inspire">
+      <v-row no-gutters>
+        <v-col cols="12">
+          <v-calendar  :attributes='attrs'></v-calendar>
+        </v-col>
+      </v-row>
+      <v-row no-gutters>
+        <v-col cols="6">
+        <v-menu
+        ref="menu"
+        v-model="menu2"
+        :close-on-content-click="false"
+        :nudge-right="40"
+        :return-value.sync="time"
+        transition="scale-transition"
+        offset-y
+        max-width="290px"
+        min-width="290px"
+      >
+        <template v-slot:activator="{ on }">
+          <v-text-field
             v-model="time"
-            full-width
-            @click:minute="$refs.menu.save(time)"
-          ></v-time-picker>
-        </v-menu>
+            label="Picker in menu"
+            prepend-icon="access_time"
+            readonly
+            v-on="on"
+          ></v-text-field>
+        </template>
+        <v-time-picker
+          v-if="menu2"
+          v-model="time"
+          full-width
+          @click:minute="$refs.menu.save(time)"
+        ></v-time-picker>
+      </v-menu>
 
-          </td>
-          <td>
-          <v-dialog
-          ref="dialog"
-          v-model="modal2"
-          :return-value.sync="time"
-          persistent
-          width="290px"
-        >
-          <template v-slot:activator="{ on }">
-            <v-text-field
-              v-model="time"
-              label="Picker in dialog"
-              prepend-icon="access_time"
-              readonly
-              v-on="on"
-            ></v-text-field>
-          </template>
-          <v-time-picker
-            v-if="modal2"
+        </v-col>
+        <v-col cols="6">
+        <v-dialog
+        ref="dialog"
+        v-model="modal2"
+        :return-value.sync="time"
+        persistent
+        width="290px"
+      >
+        <template v-slot:activator="{ on }">
+          <v-text-field
             v-model="time"
-            full-width
-          >
-            <v-spacer></v-spacer>
-            <v-btn text color="primary" @click="modal2 = false">Cancel</v-btn>
-            <v-btn text color="primary" @click="$refs.dialog.save(time)">OK</v-btn>
-          </v-time-picker>
-        </v-dialog>
-
-    
-          </td>
-        </tr>
-      </table>
+            label="Picker in dialog"
+            prepend-icon="access_time"
+            readonly
+            v-on="on"
+          ></v-text-field>
+        </template>
+        <v-time-picker
+          v-if="modal2"
+          v-model="time"
+          full-width
+        >
+          <v-spacer></v-spacer>
+          <v-btn text color="primary" @click="modal2 = false">Cancel</v-btn>
+          <v-btn text color="primary" @click="$refs.dialog.save(time)">OK</v-btn>
+        </v-time-picker>
+      </v-dialog>
 
         </v-col>
       </v-row>
-    </v-app>
+    </v-container>
       
-    </div>
+    </v-app>
       `,
       vuetify: new Vuetify(),
       data() {
