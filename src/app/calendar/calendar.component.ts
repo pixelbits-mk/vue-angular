9c1808351c65cf3c2b4e7501b38f3eae012807eb
unknown <kmike@vyana.io>
2019-11-23 12:57:35 -0500

    updated

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import dateformat from 'dateformat';
 
 declare const Vue: any;
+declare const VueTimepicker: any;
 
 @Component({
   selector: 'app-calendar',
@@ -13,23 +14,46 @@ export class CalendarComponent implements OnInit {
 
   ngOnInit() {
     Vue.customElement('vue-calendar', {
+      components: { VueTimepicker: VueTimepicker.default },
       props: [],
       template: `
       <div class="calendar">
-        <a v-on:click="prevDay()"><i class="material-icons">keyboard_arrow_left</i></a>
-        <v-date-picker :min-date="today" v-model="date" :popover="{ placement: 'bottom', visibility: 'click' }">
-          <a>
-            <i class="material-icons">date_range</i>&nbsp;&nbsp;
-            <span v-if="isToday()">TODAY</span><span v-if="!isToday()">{{ getDisplayString() }}</span>
-          </a>
-        </v-date-picker>
-        <a  v-on:click="nextDay()"><i class="material-icons">keyboard_arrow_right</i></a>
+        <div class="date">
+          <a v-on:click="prevDay()"><i class="material-icons">keyboard_arrow_left</i></a>
+          <v-date-picker :min-date="today" v-model="date" :popover="{ placement: 'bottom', visibility: 'click' }">
+            <a>
+              <i class="material-icons">date_range</i>&nbsp;&nbsp;
+              <span v-if="isToday()">TODAY</span><span v-if="!isToday()">{{ getDisplayString() }}</span>
+            </a>
+          </v-date-picker>
+          <a  v-on:click="nextDay()"><i class="material-icons">keyboard_arrow_right</i></a>
+        </div>
+        <div class="time">
+          <span class="from-to">
+            <vue-timepicker
+            :minute-range="[0]" hide-clear-button close-on-complete v-model="from" minute-interval="15" :format="format"></vue-timepicker>
+            -
+            <vue-timepicker
+            :minute-range="[0]" hide-clear-button close-on-complete v-model="to" minute-interval="15" :format="format"></vue-timepicker>
+          </span>
+        </div>
       </div>
       `,
       data() {
         return {
           date: new Date(),
-          today: new Date()
+          today: new Date(),
+          from: {
+            hh: '08',
+            mm: '00',
+            A: 'AM'
+          },
+         to: {
+            hh: '09',
+            mm: '00',
+            A: 'PM'
+          },
+          format: 'hh:mm A'
         };
       },
       methods: {
